8b8638fac5db96795f7b1ffbfda2299dadc91ad9
cyx <130837393+Elycyx@users.noreply.github.com>
2024-05-30 15:06:52 +0800

    Add files via upload

---
+++ reader_files.py
@@ -0,0 +1,29 @@
+import json
+
+def read(i):
+        """Useful to read a file content
+        Args:
+            i (int): The index of the data to read. Please avoid using any symbols, including line breaks, etc. 
+        """
+        file_name='feedback/fb.json'
+        '''如果i不是int'''
+        if not isinstance(i, int):
+            i = int(i.strip())
+        #i = int(i.strip())
+        try:
+            # 尝试从 JSON 文件读取数据
+            with open(file_name, 'r') as file:
+                data_loaded = json.load(file)
+            
+            # 检查请求的索引是否在数据范围内
+            if i < len(data_loaded):
+                result_n = data_loaded[i].get('result', '')  # 使用 get 防止 KeyError
+                success_n = data_loaded[i].get('success', '')
+                error_cause_n = data_loaded[i].get('error_cause', '')
+                return f"Result: {result_n}, Success: {success_n}, Error Cause: {error_cause_n}"
+            else:
+                # 请求的索引超出了数据范围
+                return ''
+        except (json.JSONDecodeError, FileNotFoundError):
+            # 文件为空或不是有效的 JSON，或文件不存在
+            return ''
---
+++ tf_reader.py
@@ -0,0 +1,47 @@
+import rclpy
+from rclpy.node import Node
+from tf2_ros import TransformException
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
+
+class TransformNode(Node):
+    def __init__(self):
+        super().__init__('transform_node3')
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
+        self.x = None
+        self.y = None
+        self.quit = False
+        self.create_timer(1, self.transform_callback)
+
+    def transform_callback(self):
+        try:
+            now = rclpy.time.Time()
+            trans = self.tf_buffer.lookup_transform('map', 'base_footprint', now)
+            # self.get_logger().info(f"Transform: {trans}")
+            self.x = trans.transform.translation.x
+            self.y = trans.transform.translation.y
+            self.quit = True  # Set the flag to True to exit the spin loop
+        except TransformException as ex:
+            a = 1
+            # self.get_logger().info(f'Could not transform map to base_footprint: {ex}')
+
+    def get_robot_map_position(self):
+        return (self.x, self.y)
+
+    def run(self):
+        while rclpy.ok() and not self.quit:
+            rclpy.spin_once(self, timeout_sec=0.5)
+
+def get_current_position(args=None):
+    # rclpy.init(args=args)
+    transform_node = TransformNode()
+    transform_node.run()
+    x = transform_node.x
+    y = transform_node.y
+
+    # Correctly handle the destruction of the node and shutdown of rclpy
+    transform_node.destroy_node()
+    # rclpy.shutdown()
+    return (x, y)
+
